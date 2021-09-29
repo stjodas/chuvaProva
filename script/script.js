0fc66a5936b8c781dfeee46829210776a0e287e7
stjodas <61805550+stjodas@users.noreply.github.com>
2021-09-29 20:39:26 -0300

    comit 8.1
    perdi a conta

--- script/script.js
+++ script/script.js
@@ -3,3 +3,22 @@ let list = document.querySelector('.list');
 click.addEventListener("click", ()=>{
     list.classList.toggle('newlist')
 });
+
+// Accordion
+
+let acc = document.getElementsByClassName("accordion");
+let painel = document.getElementsByClassName("")
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
